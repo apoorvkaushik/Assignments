ca9fea3175be979ee58be47e7ba25352e56a03b3
apoorvkaushik <apoorvkaushikgms@gmail.com>
2023-02-11 20:56:45 +0530

    added Q8

---
+++ Q8/prime_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    for (int i = 2; i <= 300; i++)
+    {
+        num = 0;
+
+        for (int j = 2; j <= i/2; j++)
+        {
+            if(i % j == 0)
+            {
+                num = 1;
+            }
+        }
+
+        if (num == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
