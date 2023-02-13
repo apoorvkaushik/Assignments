d88520dec954150f3e43dc0d06153917e2d62f7e
apoorvkaushik <apoorvkaushikgms@gmail.com>
2023-02-14 00:22:30 +0530

    added switch case to Q05

--- Q05/fine_check.cpp
+++ Q05/fine_check.cpp
@@ -8,27 +8,49 @@ int main()
     cout << "How much late the book is returned? (in Days): ";
     cin >> days;
 
+    switch (days)
+    {
+    case 0 ... 5:
+        cout << "Fine is 50 paise";
+        break;
+
+    case 6 ... 10:
+        cout << "Fine is 1 rupees";
+        break;
+    
+    case 11 ... 30:
+        cout << "Fine is 5 rupees";
+        break;
+    
+    default:
+        cout << "Membership is Cancelled";
+        break;
+    }
+/*  // With if and else:
+
     if (days > 0)
     {
-    if (days <= 5)
-    {
-        cout << "Fine is 50 paise";
-    }
-    else if (days <= 10)
-    {
-        cout << "Fine is 1 rupees";
-    }
-    else if (days <= 30)
-    {
-        cout << "Fine is 5 rupees";
-    }
-    else
-    {
-        cout << "Membership is Cancelled";
-    }
+        if (days <= 5)
+        {
+            cout << "Fine is 50 paise";
+        }
+        else if (days <= 10)
+        {
+            cout << "Fine is 1 rupees";
+        }
+        else if (days <= 30)
+        {
+            cout << "Fine is 5 rupees";
+        }
+        else
+        {
+            cout << "Membership is Cancelled";
+        }
     }
     else
     {
         cout << "Error: Invaild Value";
     }
+
+*/
 }
